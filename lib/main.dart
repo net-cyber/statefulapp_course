b710fcc8b7b6e984117a1e1698a8a8e072a20d33
net-cyber <ceo.natnael@gmail.com>
2022-11-05 05:25:31 +0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,9 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
-      home: HomePage(),
+      home: ApiProvider(
+        api: Api(),
+        child: HomePage(),
+      ),
     );
   }
 }
@@ -24,34 +29,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String title = 'Tap the screen';
+  ValueKey _textKey = const ValueKey<String?>(null);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(ApiProvider.of(context).api.dateAndTime ?? ''),
         ),
         body: GestureDetector(
             onTap: () {},
             child: Center(
               child: TextButton(
-                  onPressed: () {
+                  onPressed: () async {
+                    final api = ApiProvider.of(context).api;
+                    final timeAndDate = await api.getDateAndTime();
+                    //as soon as we get date and time we will update the [_textKey]
+                    setState(() {
+                      //set new value to textKey
+                      _textKey = ValueKey(timeAndDate);
+                    });
+                    //this will redraw any widget that is dependent on 
+                    //[_textKey] which is our [DateTimeWidget]
+                    //////////////////////////////////////////////////////////
                     //setState is used to redraw the widget
                     //it updates the widget after the data changes
                     // don't use Future returning function in setState
-                    setState(() {
-                      title = DateTime.now().toIso8601String();
-                    });
+                    // setState(() {
+                    //   title = DateTime.now().toIso8601String();
+                    // });
                     //setState redraws the whole screen when it called
-                    //but if you have stateless(not changing widget) 
+                    //but if you have stateless(not changing widget)
                     //on your screen it is also redrawn
                     //[inheritedWidget] gives you more controls over redraw
                     //you can compare the previous widget to the current widget
                     //therefore you have the control to redraw or not
-                    //gives you more control over how the element tree 
+                    //gives you more control over how the element tree
                     //is mapped with widget tree
                   },
-                  child: const Text('What time is it?')),
+                  child: DateTimeWidget(key: _textKey,)),
             )));
   }
 }
+
+class DateTimeWidget extends StatelessWidget {
+  const DateTimeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final api = ApiProvider.of(context).api;
+    return Text(api.dateAndTime ?? 'tap to see time');
+  }
+}
+
+class Api {
+  String? dateAndTime;
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => DateTime.now().toIso8601String(),
+    ).then((value) => dateAndTime = value);
+  }
+}
+
+class ApiProvider extends InheritedWidget {
+  final Api api;
+  final String uuid;
+  ApiProvider({
+    Key? key,
+    required this.api,
+    required Widget child,
+  })  : uuid = const Uuid().v4(),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(covariant ApiProvider oldWidget) {
+    //this is where you decide to redraw or not
+    return uuid != oldWidget.uuid;
+  }
+
+  static ApiProvider of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<ApiProvider>()!;
+}
+//! inheritedWidget is Immutable
+//so inheritedWidget is immutable how can i replace it?
+//? wrap inheritedWidget inside a StatefulWidget
+//!notice inheritedWidget redraws all child
